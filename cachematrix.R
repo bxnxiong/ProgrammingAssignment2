970359b3044f50cda4381639fa01f7ec58c359f4
bxnxiong <bxnxiong@gmail.com>
2015-03-22 00:57:08 +0800

    assi version 3

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
+## COMMENT: using this function provides you a list of the value of 
+## matrix x,setting and getting the value of variable s for inverse matrix 
+## of x(when initiated s will be NULL)
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  get <- function() x
+  setinv <- function(solve) s <<- solve
+  getinv <- function() s
+  list(get = get, setinv = setinv, 
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
+## COMMENT: the function will return inverse matrix of x, and when called twice
+## it can tell whether the matrix has been inversed or not, if inversed, it will
+## return the inversed value instead of the inversed 'inversed' one(i.e. matrix x)
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getinv()
+  if(!is.null(s)) {
+    message('getting cached data')
+    return(s)
+  }
+  m <- x$get()
+  s <- solve(m,...)
+  x$setinv(s)
+  s
 }
